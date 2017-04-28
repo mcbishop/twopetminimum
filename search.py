88507557d1c6dc24f4d5770b15b92ac409b88916
Meg Bishop <meg@hackbrightacademy.com>
2017-04-27 18:23:08 -0700

    Initial commit with searching helper functions and function stubs.

---
+++ search.py
@@ -0,0 +1,97 @@
+# OAUTH for PetFinder API.
+# For GET calls, all arguments are specified in the URL query string like this:
+ # http://api.petfinder.com/my.method?key=12345&arg1=foo&token=67890&sig=abcdef
+
+import os
+import requests
+import json
+import untangle
+import xmltodict
+import pprint
+
+key = os.environ['PETFINDER_KEY']
+secret = os.environ['PETFINDER_SECRET']
+
+payload = {'key':key, 'animal':'cat', 'location':'94110', 'count': 1000, 'offset': 100}
+
+# Send request to API and parse returned data
+
+r = requests.get(
+    "http://api.petfinder.com/pet.find?",
+    params=payload)
+
+# open and parse static dataset: with open("sample.json") as dataset: 
+
+
+my_dict = xmltodict.parse(r.text)
+
+# doc = json.dumps(my_dict)
+
+
+def is_possible_sibling(text):
+	# Identify if pet description contains mention of potential sibling or friend.
+	text_list = text.split()
+	for word in text_list:
+		if ("and" in word.lower()
+		    or "&"  in word.lower()
+		    or "brother" in word.lower() 
+			or "sister" in word.lower()
+			or "sibling"  in word.lower()
+			or "bonded"  in word.lower()
+			or "buddy"  in word.lower()
+			or "pair"  in word.lower()
+				):
+			print "############## FOUND ####################"
+			return True
+
+
+def possible_friend_name(phrase):
+	# Identify possible name of friend or sibling from description or name.
+
+
+	pass
+
+def all_pet_results(pet_dict):
+	pet_names = {}
+	for pet_dict in my_dict['petfinder']['pets'].values():
+		for pet in pet_dict:
+			# print pet['name']
+			# print "+++++++++++++++++++", pet['id'], "++++++++++++++++"
+			if is_possible_sibling(pet['name']) is True:
+				pet_names[pet['name']] = []
+		
+				# if possible_friend_name(pet['name']) is True:
+				# 	print "I might have a friend"
+
+				# print pet
+				
+		print len(pet_names)
+		print pet_names
+
+all_pet_results(my_dict)
+
+
+
+def find_friend(name, pet_dict):
+	# Search same-shelter listings for a pet with matching friend/sibling name.
+	# Input: pet dictionary including each pet's shelter
+	# Identify listings from same shelter
+	# exclude originating pet, search for sibling name
+	pass
+
+def dedupe_results(text):
+	# Dedupe set of results as shelters list same two pets in different order.
+	pass
+
+
+
+def is_affectionate(description):
+	# check phrase for snuggle words
+	# for word in description:
+	# 	if ("lap" or "snuggle" or "hug"):
+		# for each word, assign a point, sum will equal affection rating 
+	pass
+
+def affection_rating(description):
+	# sentiment analysis 
+	pass
