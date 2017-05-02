f853980e3a2e24f0408e392ea946c27b1734ddff
Meg Bishop <meg@hackbrightacademy.com>
2017-05-01 17:45:56 -0700

    Cleanup for PEP 8 standards.

--- search.py
+++ search.py
@@ -4,94 +4,78 @@
 
 import os
 import requests
-import json
-import untangle
 import xmltodict
-import pprint
+
 
 key = os.environ['PETFINDER_KEY']
 secret = os.environ['PETFINDER_SECRET']
 
 
+def get_current_pets(payload):
+    """ Send request to API return dictionary of pet search results"""
 
-def get_current_pets(terms):
-	""" Send request to API return dictionary of pet search results"""
+    r = requests.get(
+        "http://api.petfinder.com/pet.find?",
+        params=payload)
 
-
-	r = requests.get(
-	    "http://api.petfinder.com/pet.find?",
-	    params=payload)
-
-
-	pet_dict = xmltodict.parse(r.text)
-
-	return pet_dict
+    pet_dict = xmltodict.parse(r.text)
+    return pet_dict
 
 
-def is_possible_sibling(text):
-	# Identify if pet description contains mention of potential sibling or friend.
-	text_list = text.split()
-	for word in text_list:
-		word = word.lower()
-		for search_word in SEARCH_TERMS:
-			if word == search_word:
-				print "############## FOUND ####################"
-				return True
+def is_possible_sibling(text, phrases):
+    # Identify if pet description contains mention of potential sibling or friend.
+    text_list = text.split()
+    for word in text_list:
+        word = word.lower()
+        if word in phrases:
+                return True
 
 
 def possible_friend_name(phrase):
-	# Identify possible name of friend or sibling from description or name.
-	# Disregard root pet name.
+    # Identify possible name of friend or sibling from description or name.
+    # Disregard root pet name.
+    pass
 
 
-	pass
-
-def all_pet_results(pet_dict):
-	pet_names = {}
-	for pet_dict in my_dict['petfinder']['pets'].values():
-		for pet in pet_dict:
-			# print pet['name']
-			# print "+++++++++++++++++++", pet['id'], "++++++++++++++++"
-			if is_possible_sibling(pet['name']) is True:
-				pet_names[pet['name']] = []
-				# if possible_friend_name(pet['name']) is True:
-				# 	print "I might have a friend"
-
-				# print pet
-				
-		print len(pet_names)
-		return pet_names
+def all_pet_results(all_pets, phrases):
+    pet_names = {}
+    for pet_dict in all_pets['petfinder']['pets'].values():
+        for pet in pet_dict:
+            if is_possible_sibling(pet['name'], phrases):
+                pet_names[pet['name']] = []          
+        print len(pet_names)
+        return pet_names
 
 
 def find_friend(name, pet_dict):
-	# Search same-shelter listings for a pet with matching friend/sibling name.
-	# Input: pet dictionary including each pet's shelter
-	# Identify listings from same shelter
-	# exclude originating pet, search for sibling name
-	pass
+    # Search same-shelter listings for a pet with matching friend/sibling name.
+    # Input: pet dictionary including each pet's shelter
+    # Identify listings from same shelter
+    # exclude originating pet, search for sibling name
+    pass
+
 
 def dedupe_results(text):
-	# Dedupe set of results as shelters list same two pets in different order.
-	pass
-
+    # Dedupe set of results as shelters list same two pets in different order.
+    pass
 
 
 def is_affectionate(description):
-	# check phrase for snuggle words
-	# for word in description:
-	# 	if ("lap" or "snuggle" or "hug"):
-		# for each word, assign a point, sum will equal affection rating 
-	pass
+    # check phrase for snuggle words
+    # for word in description:
+    #   if ("lap" or "snuggle" or "hug"):
+        # for each word, assign a point, sum will equal affection rating
+    pass
+
 
 def affection_rating(description):
-	# sentiment analysis 
-	pass
+    # sentiment analysis
+    pass
 
 
-payload = {'key':key, 'animal':'cat', 'location':'94110', 'count': 1000, 'offset': 100}
-
-SEARCH_TERMS = ["and","&","brother","sister","sibling","bonded","buddy","pair"]
-pet_dict = get_current_pets(payload)
-print all_pet_results(pet_dict)
+test_search_terms = {'key': key, 'animal': 'cat', 'location': '94110', 'count': 1000, 'offset': 100}
+pair_phrases = ["and", "&", "brother", "sister", "sibling", "bonded", "buddy", "pair"]
+current_pets = get_current_pets(test_search_terms)
+print all_pet_results(current_pets, pair_phrases)
 
 
