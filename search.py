fe74782bfa1619a9b440edfe5d9e0ba10e33dc79
Meg Bishop <meg@hackbrightacademy.com>
2017-05-03 11:38:24 -0700

    Added simple check for sentiment.

--- search.py
+++ search.py
@@ -41,8 +41,12 @@ def all_pet_results(all_pets, phrases):
     pet_names = {}
     for pet_dict in all_pets['petfinder']['pets'].values():
         for pet in pet_dict:
+            # If pet name includes possible sibling, make a dictionary entry
             if is_possible_sibling(pet['name'], phrases):
-                pet_names[pet['name']] = []          
+                pet_names[pet['name']] = []
+                # If description includes affection keywords, append dictionary value
+                if is_affectionate(pet['description']):
+                    pet_names[pet['name']] = [pet['description']]  
         print len(pet_names)
         return pet_names
 
@@ -55,14 +59,20 @@ def find_friend(name, pet_dict):
     pass
 
 
-def is_affectionate(affection_phrases, text):
-    """Input: origin text, affection phrases; output: boolean."""
+def is_affectionate(text):
+    """Input: origin text; output: boolean."""
+    affection_phrases = ["snuggle", "cuddly", "lap", "hug", "affectionate", "friendly"]
     text_list = text.split()
     for word in text_list:
         word = word.lower()
         if word in affection_phrases:
             return True
 
+
+
+def word_frequency(blob):
+    """Input: blob, output: word frequency."""
+    pass
 
 def affectionate_pets():
     """ Input: dictionary of pets, output: pet names and descriptions of
@@ -72,8 +82,9 @@ def affectionate_pets():
 
 test_search_terms = {'key': key, 'animal': 'cat', 'location': '94110', 'count': 1000, 'offset': 100}
 pair_phrases = ["and", "&", "brother", "sister", "sibling", "bonded", "buddy", "pair"]
-affection_phrases = ["snuggle", "cuddly", "lap", "hug", "affectionate", "friendly", ]
+
 current_pets = get_current_pets(test_search_terms)
+
 print all_pet_results(current_pets, pair_phrases)
 
 
