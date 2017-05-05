7f15d635d6aaa03190eedc24fc296745091cc5c5
Meg Bishop <meg@hackbrightacademy.com>
2017-05-04 17:26:03 -0700

    Edits to server and added future todos.

--- server.py
+++ server.py
@@ -9,5 +9,22 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route('/search')
+def search():
+    """Return search results."""
+
+    # Query database for pets and shelters.
+    # Display pets on the page.
+
+    pass
+
+
+@app.route('/shelters')
+def display_shelter_pets():
+    """ Display d3 chart of pets in a given shelter."""
+
+    pass
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
