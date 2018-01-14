5a07016a90b7c267c0738636c784217898aa58ad
Meg Chilton <810585+mcbishop@users.noreply.github.com>
2018-01-13 19:14:50 -0800

    Began working on map.

--- static/custom.js
+++ static/custom.js
@@ -17,23 +17,28 @@ function getPetInfo(evt) {
 
 function getShelterInfo(evt) {
       console.log(evt);
-    var pet_id = $(this).data('shelter-id');
-    console.log("Pet ID is", shelter_id);
+    var shelter_id = $(this).data('shelterId');
+    console.log("Shelter ID is", shelter_id);
 
     $.get("/shelter.json", {'shelter_id':shelter_id},
         function(results) {
             console.log("Made it! Results are", results);
             var shelter_id = results.id;
-            var shelter_desc = results.description;
-            console.log(shelter_desc);
+            var name = results.name;
+            var lat = results.latitude;
+            var lng = results.longitude;
+            var phone = results.phone;
+            var email = results.email;
+            console.log(email);
             
-            $('#div_' + shelter_id).html(shelter_desc);
+            $('#div_' + shelter_id).html(email);
         });
 }  
-}
+
 
 
 $('.button').click(getPetInfo);
+
 $('.button shelter').click(getShelterInfo);
 
 
