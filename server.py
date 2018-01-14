5de9e2f295776c3015ba20751b987a8580790509
Meg Chilton <810585+mcbishop@users.noreply.github.com>
2018-01-14 10:46:49 -0800

    Testing out Twilio's SMS demo feature.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, url_for
 from flask_googlemaps import GoogleMaps, Map
 from flask.ext.sqlalchemy import SQLAlchemy
 from model import Pet, Shelter, Photo, connect_to_db, db
 import seed
 import json
+from twilio.twiml.voice_response import VoiceResponse
+from twilio.rest import Client
 
 import os
 google_key = os.environ['GOOGLE_SECRET']
@@ -94,6 +96,62 @@ def display_shelter_pets():
     """ Display pets in a given shelter."""
 
     pass
+
+# Below code is from Twilio tutorial, https://github.com/TwilioDevEd/clicktocall-flask/
+
+
+# Declare and configure application
+app = Flask(__name__, static_url_path='/static')
+app.config.from_pyfile('local_settings.py')
+
+
+
+# Text Request URL
+@app.route('/call', methods=['POST'])
+def call():
+    # Get phone number we need to call
+    phone_number = request.form.get('phoneNumber', None)
+
+    try:
+        twilio_client = Client(app.config['TWILIO_ACCOUNT_SID'],
+                               app.config['TWILIO_AUTH_TOKEN'])
+    except Exception as e:
+        msg = 'Missing configuration variable: {0}'.format(e)
+        return jsonify({'error': msg})
+
+    try:
+        twilio_client.calls.create(from_=app.config['TWILIO_CALLER_ID'],
+                                   to=phone_number,
+                                   url=url_for('.outbound',
+                                               _external=True))
+    except Exception as e:
+        app.logger.error(e)
+        return jsonify({'error': str(e)})
+
+    return jsonify({'message': 'Call incoming!'})
+
+
+@app.route('/outbound', methods=['POST'])
+def outbound():
+    response = VoiceResponse()
+
+    response.say("Thank you for contacting our sales department. If this "
+                 "click to call application was in production, we would "
+                 "dial out to your sales team with the Dial verb.",
+                 voice='alice')
+    '''
+    # Uncomment this code and replace the number with the number you want
+    # your customers to call.
+    response.number("+16518675309")
+    '''
+    return str(response)
+
+
+# Route for Landing Page after Heroku deploy.
+@app.route('/landing.html')
+def landing():
+    return render_template('landing.html',
+                           configuration_error=None)
 
 
 
